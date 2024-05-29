72683f7451aba3be105de8d5f2084cf4a1646461
Salem <svibyrne@gmail.com>
2024-05-29 13:48:50 +0200

    Slight refactoring

--- foldch.py
+++ foldch.py
@@ -21,6 +21,7 @@ parser.add_argument('input_file', type=str)
 parser.add_argument('--reference_sample', '--rs', type=str, default='H1975 par')
 parser.add_argument('--reference_target', '--rt', type=str, default='28S')
 
+
 def main(args: Arguments):
     input_df = pd.read_excel(args.input_file)
 
@@ -32,17 +33,20 @@ def main(args: Arguments):
     
     output_df = pd.DataFrame(combinations, columns=['Sample', 'Target'])
     
+    def get_group(df: pd.DataFrame, sample: str, target: str) -> pd.DataFrame:
+        return df.groupby(['Sample', 'Target']).get_group((sample, target))
+    
     # Calculated from input
-    output_df['Ct Avg'] = output_df.apply(lambda x: input_df.groupby(['Sample', 'Target']).get_group((x['Sample'], x['Target']))['Cq'].mean(), axis=1)
-    output_df['Ct Std'] = output_df.apply(lambda x: input_df.groupby(['Sample', 'Target']).get_group((x['Sample'], x['Target']))['Cq'].std(), axis=1)
-    output_df['Ct Avg Ctrl'] = output_df.apply(lambda x: input_df.groupby(['Sample', 'Target']).get_group((x['Sample'], args.reference_target))['Cq'].mean(), axis=1)
-    output_df['Ct Std Ctrl'] = output_df.apply(lambda x: input_df.groupby(['Sample', 'Target']).get_group((x['Sample'], args.reference_target))['Cq'].std(), axis=1)
+    output_df['Ct Avg'] = output_df.apply(lambda x: get_group(input_df, x['Sample'], x['Target'])['Cq'].mean(), axis=1)
+    output_df['Ct Std'] = output_df.apply(lambda x: get_group(input_df, x['Sample'], x['Target'])['Cq'].std(), axis=1)
+    output_df['Ct Avg Ctrl'] = output_df.apply(lambda x: get_group(input_df, x['Sample'], args.reference_target)['Cq'].mean(), axis=1)
+    output_df['Ct Std Ctrl'] = output_df.apply(lambda x: get_group(input_df, x['Sample'], args.reference_target)['Cq'].std(), axis=1)
 
     # Calculated from intermediate values
     output_df['Delta Ct'] = output_df['Ct Avg'] - output_df['Ct Avg Ctrl']
     output_df['Delta Ct Std'] = np.sqrt((output_df['Ct Std'] ** 2) + (output_df['Ct Std Ctrl'] ** 2))
     
-    output_df['Delta Ct Avg Ctrl Sample'] = output_df.apply(lambda x: output_df.groupby(['Sample', 'Target']).get_group((args.reference_sample, x['Target']))['Delta Ct'].mean(), axis=1)
+    output_df['Delta Ct Avg Ctrl Sample'] = output_df.apply(lambda x: get_group(output_df, args.reference_sample, x['Target'])['Delta Ct'].mean(), axis=1)
     output_df['Delta Delta Ct'] = output_df['Delta Ct'] - output_df['Delta Ct Avg Ctrl Sample']
     
     output_df['Fold'] = 2 ** (-output_df['Delta Delta Ct'])
